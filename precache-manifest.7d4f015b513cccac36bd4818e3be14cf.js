5ab86d4c241d3695aa26ce7d3ad3c54493036c77
trev-james <trevor@thienhardt.com>
2022-01-28 22:37:37 -0600

    Updates

---
+++ precache-manifest.7d4f015b513cccac36bd4818e3be14cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb06b0d3e362ae6ee6c040069995b493",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dabfbe3da243133f600a",
+    "url": "/monsters-rolodex/static/css/main.f223d494.chunk.css"
+  },
+  {
+    "revision": "c8e56e1d6cf27670d4ed",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef76e7449ec7d375dc7",
+    "url": "/monsters-rolodex/static/js/3.78ba26aa.chunk.js"
+  },
+  {
+    "revision": "dabfbe3da243133f600a",
+    "url": "/monsters-rolodex/static/js/main.76bae4ce.chunk.js"
+  },
+  {
+    "revision": "44d58ac42f2c240abb6e",
+    "url": "/monsters-rolodex/static/js/runtime-main.a07f12a2.js"
+  }
+]);
